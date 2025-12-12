5a7dda1282045129d675cb819cbcab9f055be7f4
Santi <44323385+Despampanante@users.noreply.github.com>
2025-12-12 00:10:15 -0500

    moved a bunch of stuff around, probably goign to crash and burn lol

--- config/keypos_def/keypos_34keys.h
+++ config/keypos_def/keypos_34keys.h
@@ -77,9 +77,52 @@
 #define RHI 32  // right thumb keys
 #define RHO 33
 
-#define KEYS_L LTP LTR LTM LTI LTC LMP LMR LMM LMI LMC LBP LBR LBM LBI LBC  // left-hand keys
-#define KEYS_R RTC RTI RTM RTR RTP RMC RMI RMM RMR RMP RBC RBI RBM RBR RBP  // right-hand keys
-#define THUMBS     LHI LHO RHI RHO                                          // thumb keys
+// Numeric position aliases (0-33)
+#define LT0  0   // left-top row
+#define LT1  1
+#define LT2  2
+#define LT3  3
+#define LT4  4
+
+#define RT0  5   // right-top row
+#define RT1  6
+#define RT2  7
+#define RT3  8
+#define RT4  9
+
+#define LM0 10   // left-middle row
+#define LM1 11
+#define LM2 12
+#define LM3 13
+#define LM4 14
+
+#define RM0 15   // right-middle row
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+#define LB0 20   // left-bottom row
+#define LB1 21
+#define LB2 22
+#define LB3 23
+#define LB4 24
+
+#define RB0 25   // right-bottom row
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+
+#define LH0 30   // left thumb keys
+#define LH1 31
+
+#define RH0 32   // right thumb keys
+#define RH1 33
+
+#define KEYS_L LTP LTR LTM LTI LTC LMP LMR LMM LMI LMC LBP LBR LBM LBI LBC LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left-hand keys
+#define KEYS_R RTC RTI RTM RTR RTP RMC RMI RMM RMR RMP RBC RBI RBM RBR RBP RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right-hand keys
+#define THUMBS LHI LHO RHI RHO LH0 LH1 RH0 RH1                                                                                              // thumb keys
 
 #define LAYER_FROM36( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
@@ -91,3 +134,12 @@
     k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  \
     k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  \
                    k31  k32  k33  k34
+
+/*                KEY POSITIONS
+
+  ╭─────────────────────╮ ╭─────────────────────╮
+  │ LT4 LT3 LT2 LT1 LT0 │ │ RT0 RT1 RT2 RT3 RT4 │
+  │ LM4 LM3 LM2 LM1 LM0 │ │ RM0 RM1 RM2 RM3 RM4 │
+  │ LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 │
+  ╰───────╮ LH2 LH1 LH0 │ │ RH0 RH1 RH2 ╭───────╯
+          ╰─────────────╯ ╰─────────────╯ */
---
+++ config/newStruc/stuff.md
@@ -0,0 +1,17 @@
+asdfasdf
+
+# Stuff I don't like
+
+1. All second layer where new layer goes between keys. "X l2 X", Rather than the prefered "l2 X X"
+   1. Ex
+      1. By <- B is on base layer, y is on second layer. Interupts roll
+      2. syo
+      3. sk
+      4. ck
+   2. Stretches
+      1. pl
+      2. pr
+   3. SFBs
+      1. rn
+      2. tl
+2. 
